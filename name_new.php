18c1488f13831696890bd2308782ff8e227c5d54
Kishankanani1997 <114581237+Kishankanani1997@users.noreply.github.com>
2022-11-15 14:40:59 +0530

    Delete name_new.php

--- name_new.php
+++
@@ -1,5 +0,0 @@
-<?=  
-$name="hi i am kishan"; // If we write = instead of php at the starting then it will only print single value. 
-                        // = prints only single value so we do not use =.
-$name1="i am leraning html/css";
-?>
