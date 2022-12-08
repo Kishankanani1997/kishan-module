5f0f7abba3f7eea5269f35479b71a46612ce0536
Kishankanani1997 <114581237+Kishankanani1997@users.noreply.github.com>
2022-12-08 17:57:28 +0530

    Delete Module_3/json directory

--- Module_3/json/city_list.php
+++
@@ -1,4 +0,0 @@
-<?php
-$city=array("rj"=>"Rajkot","su"=>"Surat","nv"=>"Navsari","mb"=>"Morbi","mu"=>"Mumbai","ku"=>"Kutch","va"=>"Vapi","ud"=>"Udaipur");
-echo json_encode($city);
-?>
--- Module_3/json/state_list.php
+++
@@ -1,4 +0,0 @@
-<?php
-$state=array("guj"=>"Gujarat","mh"=>"Maharashtra","ka"=>"Karnataka","bh"=>"Bihar");
-echo json_encode($state);
-?>
